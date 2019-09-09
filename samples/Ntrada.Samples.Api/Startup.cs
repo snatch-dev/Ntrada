d8ad20a30bcd168202ac098a3e5c03e87052fe32
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-09-09 19:22:10 +0200

    tests, samples

---
+++ samples/Ntrada.Samples.Api/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ntrada.Samples.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddNtrada();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseNtrada();
+        }
+    }
+}
