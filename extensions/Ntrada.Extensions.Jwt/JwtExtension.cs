bb6df80b43b6375e912cf1527d04ce17eb1124ac
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-12-20 20:47:22 +0100

    JWT update

--- extensions/Ntrada.Extensions.Jwt/JwtExtension.cs
+++ extensions/Ntrada.Extensions.Jwt/JwtExtension.cs
@@ -16,21 +16,68 @@ namespace Ntrada.Extensions.Jwt
         {
             var options = optionsProvider.GetForExtension<JwtOptions>(Name);
             services.AddAuthorization();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(cfg =>
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                RequireAudience = options.RequireAudience,
+                ValidIssuer = options.ValidIssuer,
+                ValidIssuers = options.ValidIssuers,
+                ValidateActor = options.ValidateActor,
+                ValidAudience = options.ValidAudience,
+                ValidAudiences = options.ValidAudiences,
+                ValidateAudience = options.ValidateAudience,
+                ValidateIssuer = options.ValidateIssuer,
+                ValidateLifetime = options.ValidateLifetime,
+                ValidateTokenReplay = options.ValidateTokenReplay,
+                ValidateIssuerSigningKey = options.ValidateIssuerSigningKey,
+                SaveSigninToken = options.SaveSigninToken,
+                RequireExpirationTime = options.RequireExpirationTime,
+                RequireSignedTokens = options.RequireSignedTokens,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            if (!string.IsNullOrWhiteSpace(options.AuthenticationType))
+            {
+                tokenValidationParameters.AuthenticationType = options.AuthenticationType;
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.IssuerSigningKey))
+            {
+                tokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(options.IssuerSigningKey));
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.NameClaimType))
+            {
+                tokenValidationParameters.NameClaimType = options.NameClaimType;
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.RoleClaimType))
+            {
+                tokenValidationParameters.RoleClaimType = options.RoleClaimType;
+            }
+
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(o =>
                 {
-                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(o =>
+                {
+                    o.Authority = options.Authority;
+                    o.Audience = options.Audience;
+                    o.MetadataAddress = options.MetadataAddress;
+                    o.SaveToken = options.SaveToken;
+                    o.RefreshOnIssuerKeyNotFound = options.RefreshOnIssuerKeyNotFound;
+                    o.RequireHttpsMetadata = options.RequireHttpsMetadata;
+                    o.IncludeErrorDetails = options.IncludeErrorDetails;
+                    o.TokenValidationParameters = tokenValidationParameters;
+                    if (!string.IsNullOrWhiteSpace(options.Challenge))
                     {
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.Key)),
-                        ValidIssuer = options.Issuer,
-                        ValidIssuers = options.Issuers,
-                        ValidAudience = options.Audience,
-                        ValidAudiences = options.Audiences,
-                        ValidateIssuer = options.ValidateIssuer,
-                        ValidateAudience = options.ValidateAudience,
-                        ValidateLifetime = options.ValidateLifetime,
-                        ClockSkew = TimeSpan.Zero
-                    };
+                        o.Challenge = options.Challenge;
+                    }
                 });
         }
 
